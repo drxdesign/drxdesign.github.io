de8985e651f84395b4069eb30a55f541a1bc6bdd
Ziane Maamar <drx-design@hotmail.com>
2024-12-31 18:46:51 +0100

    First build version 0.0.1
    Finally finished the whole process of building the html and css still some minor tweak to do but I'm just trying to make sure everything is working properly

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,19 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
-	},
+		extend: {
+		  colors: {
+			primary: '#D1FF00', // Kiwi Green
+			background: '#0F1318', // Dark Navy
+			highlight: '#E4FF33', // Lighter Kiwi Green
+			text: '#E8E8E8', // Light Gray
+		  },
+		  fontFamily: {
+			sans: ['Poppins', 'sans-serif'], // Replace 'Poppins' with Montserrat or Inter if preferred
+		  },
+		},
+	  },
 	plugins: [],
 }
+
+  
